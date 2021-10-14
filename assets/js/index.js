84a8ddf10fa88cf57b9987444ae49241ed38fcd2
Michael Rodriguez <mikeyrod22@gmail.com>
2021-10-14 11:22:15 -0500

    api call accepts a city search, catches errors, receives relevant data, and assigns data to properties of a passed object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// EVENT LISTENERS
 function toggleHistoryOpen() {
   document.querySelector(".history").classList.toggle("open");
   const icon = document.querySelector("#history-toggle-icon");
@@ -5,3 +6,37 @@ function toggleHistoryOpen() {
     ? (icon.className = "fa fa-angle-up")
     : (icon.className = "fa fa-angle-down");
 }
+
+// API CALL
+async function apiCall(passedSearch, result) {
+  const key = "219eeebee89bb95abff246f577c10545";
+  //   let searchResponse = {};
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${passedSearch}&appid=${key}`
+  )
+    .then((cityResponse) => {
+      return cityResponse.status === 200
+        ? cityResponse.json()
+        : Promise.reject("No city was found with this name.");
+    })
+    .then((cityData) => {
+      console.log(cityData);
+      result.name = cityData.name;
+      const cityLat = cityData.coord.lat;
+      const cityLon = cityData.coord.lon;
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${key}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          result.current = data.current;
+          result.daily = data.daily;
+        });
+    })
+    .catch((err) => window.alert(err));
+}
+
+const result = {};
+apiCall("dallas", result);
+console.log("result: ", result);
